0a26c1796c8a44a5a2ac884a1973adeff2a790a7
donkennie <ajeigbekehinde160@gmail.com>
2022-11-02 03:59:02 +0100

    add bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,33 @@ class ArticlesController < ApplicationController
 
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+        flash[:notice] = "Article was updated"
+        redirect_to article_path(@article)
+        else
+        flash[:notice] = "Article was not updated"
+        render 'edit'
+        end
+    end
+
+
+    def index
+        @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def show
         @article = Article.find(params[:id])
     end
@@ -23,5 +50,10 @@ class ArticlesController < ApplicationController
        def article_params
         params.require(:article).permit(:title, :description)
        end
+
+       def set_article
+            @article = Article.find(params[:id])
+        end
+
        
 end
